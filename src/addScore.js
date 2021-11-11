e0bbd19f1085d5a1d5072f86c13d9bfde144f161
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2021-11-11 01:01:21 +0100

    changed getScores to use async and await

--- src/addScore.js
+++ src/addScore.js
@@ -1,19 +1,32 @@
+import scores from './leaderbord'
+
 export default 'Olympics';
 const gameId = 'SJ6sb5XLeWomNhN0de2m';
 const baseUrl = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api';
 const requestUrl = `${baseUrl}/games/${gameId}/scores`;
 
+
 const requestHeader = {
   'Content-type': 'application/json; charset=UTF-8',
 };
 
-export const getScores = () => fetch(requestUrl, {
-  method: 'GET',
-})
-  .then((response) => response.json())
-  .then((data) => data.result.sort(
-    (score1, score2) => score2.score - score1.score,
-  ));
+
+export const getScores = async () => {
+  const request = await fetch(requestUrl)
+  const response = await request.json()
+  return response.result.sort((score1, score2) => score2.score -score1.score)
+  
+}
+
+
+
+
+
+// export const submitScore = async (user, score) =>  {
+//   const request = await fetch(requestUrl),
+//   body = JSON.stringify({ user, score })
+
+// }
 
 export const submitScore = async (user, score) => fetch(requestUrl, {
   method: 'POST',
